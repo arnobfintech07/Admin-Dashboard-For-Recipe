791151404a5ace22ad900be15f56f5ecd5fd15c0
arnobfintech07 <arnob@fintech-point.com>
2026-01-07 18:53:40 +0600

    SideBar Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Components/Navbar'
+import SideBar from './Components/SideBar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Navbar/>
+      <SideBar/>
     </>
   )
 }
---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,26 @@
+function SideBar() {
+  return (
+    
+    <ul className=" my-5 menu bg-base-200  w-56 [&_li>*]:rounded-none p-0">
+    <div className=" text-xl">
+      <li>
+        <a>Dashboard</a>
+      </li>
+      <li>
+        <a>Categories</a>
+      </li>
+      <li>
+        <a>Settings</a>
+      </li>
+      <li>
+        <a>Recipies</a>
+      </li>
+      <li>
+        <a>Sign Out</a>
+      </li>
+      </div>
+    </ul>
+
+  );
+}
+export default SideBar;
