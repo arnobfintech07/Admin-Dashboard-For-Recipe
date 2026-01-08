0de4a5690c94c3f3976f3bc7cc866f9fce90097c
arnobfintech07 <arnob@fintech-point.com>
2026-01-08 16:44:09 +0600

    Recipes Card images added

--- src/Pages/Card.jsx
+++ src/Pages/Card.jsx
@@ -1,21 +1,18 @@
-function Card(){
-     return(
-        <>
-       <div className="m-5 card bg-base-100 w-80 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
-        </>
-     )
+function Card() {
+  return (
+    <>
+      <div className="border-amber-100 shadow m-5 card bg-base-100 w-80  hover:cursor-grab hover:shadow-xl">
+        <figure>
+          <img
+            src="https://recipeapp.neurondigital.com/uploads/1537472654recipe_0000_vitchakorn-koonyosying-494170-unsplash.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Fresh Muscles</h2>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Card
+export default Card;
