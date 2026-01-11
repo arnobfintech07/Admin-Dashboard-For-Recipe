b260d1c8c62a836f5b6a548da0331778d79defca
arnobfintech07 <arnob@fintech-point.com>
2026-01-11 14:40:44 +0600

    Recipe Name,Media Directions Added

--- src/Pages/RecipeModal.jsx
+++ src/Pages/RecipeModal.jsx
@@ -1,31 +1,74 @@
-import React from 'react'
+import React from "react";
 
 const RecipeModal = () => {
   return (
-  <>
-  <div className="card bg-base-100 w-96 shadow-sm m-57 h-200 ">
-  <div className='min-h-0'>
-    <figure>
-    <h1 className="bg-amber-200 text-2xl rounded shadow w-full pl-5 p-3 min-h-0">Edit Recipe</h1>
-  </figure>
-  <div>
-    <p className='pl-5 mt-6 font-bold pb-2'>Recipe Name</p>
-  </div>
-  <div className='pl-5'>
-    <input type="text" placeholder="eg: Chicken Soup" className="input " />
-  </div>
-  <div className="card bg-base-100 w-80 shadow-sm h-40 m-5  ">
-  <h2 className='bg-base-200 pl-5 p-3 rounded'>Media</h2>
-  <div>
-    <h1 className='p-4'>Recipe Images</h1>
-  </div>
-  
-</div>
-  </div>
-</div>
+    <>
+      <div className="card bg-base-100 w-110 shadow-sm m-57 h-200 ">
+        <div className="min-h-0">
+          <figure>
+            <h1 className="bg-amber-200 text-2xl rounded shadow w-full pl-5 p-3 min-h-0">
+              Edit Recipe
+            </h1>
+          </figure>
+          <div>
+            <p className="pl-5 mt-6 font-bold pb-3 mb-3">Recipe Name</p>
+          </div>
 
-  </>
-  )
-}
+          <div>
+            <div className="pl-5">
+            <input
+              type="text"
+              placeholder="eg: Chicken Soup"
+              className="input w-100 "
+            />
+          </div>
+        </div>
+        
+          <div className="card bg-base-100 h-72 w-100 shadow-sm m-5   ">
+            <h2 className="bg-base-200 pl-5 p-3 rounded">Media</h2>
+            <div>
+              <h1 className="p-4">Recipe Images</h1>
+            </div>
+            <div className="pl-4 ">
+              <input type="file" className=" file-input w-90" />
+            </div>
+            <div className="">
+                <h1 className="p-4">Description</h1>
+            </div>
+            <div className="pl-4 ">
+              <input type="text" placeholder="Give Description" className="pl-3 file-input w-90" />
+            </div>
+          </div>
+          </div>
+        <div className="mt-20 ">
+          <div className="card bg-base-100 w-100 shadow-sm h-70 m-5  ">
+            <h2 className="bg-base-200 pl-5 p-3 rounded">Recipe Directions</h2>
+            <div>
+              <h1 className="p-4">Prep. Time(min)</h1>
+            </div>
+            <div className="pl-5">
+              <input
+                type="text"
+                placeholder="eg: give time in min"
+                className="input w-90 "
+              />
+            </div>
+            <div className="">
+              <h1 className="mt-3 pl-4">Cook Time (min)</h1>
 
-export default RecipeModal
+              <div className="mt-3 pl-5">
+                <input
+                  type="text"
+                  placeholder="eg: give time in min"
+                  className="input w-90 "
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RecipeModal;
