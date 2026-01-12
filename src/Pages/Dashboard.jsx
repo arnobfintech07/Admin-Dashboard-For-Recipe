132241aab0463fbc6b71efad228eb9f091a2d8e3
arnobfintech07 <arnob@fintech-point.com>
2026-01-12 12:39:05 +0600

    Popular Recipe Added in Dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import CardModal from "../Components/CardModal";
 import Navbar from "../Components/Navbar";
 import SideBar from "../Components/SideBar";
 function Dashboard() {
@@ -8,20 +9,13 @@ function Dashboard() {
         <SideBar />
         <div className="m-5 flex-1  h-4">
           <h1 className=" text-3xl rounded shadow">DashBoard</h1>
-          <div className="card bg-base-100 h-100 w-96 shadow-sm m-5">
-            <div className="bg-amber-200">
-                  <h1 className=" text-xl rounded shadow">Top Viewed</h1>
+          <div className="card bg-base-100 h-150 w-110 shadow-sm m-5">
+            <div className="bg-amber-200 pl-4 h-10 ">
+                  <h1 className=" text-2xl  ">Popular</h1>
             
             </div>
-            <div className="card-body">
-              <h2 className="card-title">Card Title</h2>
-              <p>
-                A card component has a figure, a body part, and inside body
-                there are title and actions parts
-              </p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Buy Now</button>
-              </div>
+            <div>
+              <CardModal/>
             </div>
           </div>
         </div>
