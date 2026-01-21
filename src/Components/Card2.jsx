33e98d814a0b3072d140e4af61b56faf92ef6c94
arnobfintech07 <arnob@fintech-point.com>
2026-01-21 11:11:06 +0600

    The three cards of Dashboard is dynamic

--- src/Components/Card2.jsx
+++ src/Components/Card2.jsx
@@ -4,9 +4,22 @@ import { MdOutlineCategory } from "react-icons/md";
 import { IoSettingsSharp } from "react-icons/io5";
 import { MdReviews } from "react-icons/md";
 import Table from "./Table";
+import { useEffect, useState } from "react";
 
-const Card2 = ({cardValue}) => {
-//   const cardValue = 500;
+const Card2 = () => {
+  // Initialize with 0 or empty to prevent "undefined" errors
+  const [stats, setStats] = useState({
+    recipeCount: 0,
+    categoryCount: 0,
+    commentsCount: 0,
+  });
+   useEffect(() => {
+    fetch("http://192.168.0.249:8000/api/dashboard")
+      .then((res) => res.json())
+      .then((data) => setStats(data))
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
+  
 
   return (
     <>
@@ -21,7 +34,7 @@ const Card2 = ({cardValue}) => {
           
         </div>
         <div>
-          <span className="text-xl lg:text-6xl">100</span>
+          <span className="text-xl lg:text-6xl">{stats.recipeCount}</span>
         </div>
       </div>
       <div className="bg-base-300 w-50 lg:w-120 rounded-3xl p-6 flex flex-col gap-4">
@@ -34,7 +47,7 @@ const Card2 = ({cardValue}) => {
           </span>
         </div>
         <div>
-          <span className="text-xl lg:text-6xl">10</span>
+          <span className="text-xl lg:text-6xl"> {stats.categoryCount}</span>
         </div>
       </div>
       <div className="bg-base-300 w-50 lg:w-120 rounded-3xl p-6 flex flex-col gap-4">
@@ -47,7 +60,7 @@ const Card2 = ({cardValue}) => {
           </span>
         </div>
         <div>
-          <span className="text-xl lg:text-6xl">50</span>
+          <span className="text-xl lg:text-6xl"> {stats.commentsCount}</span>
         </div>
       </div>
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div className="sticky top-0 z-50 navbar bg-base-100 shadow">
       <div className="flex pr-100 text-left">
-        <a className="btn btn-ghost text-xl">Admin Dashboard</a>
+        <Link to= "/dashboard" className="btn btn-ghost text-xl">Admin Dashboard</Link>
       </div>
       <div className="flex mt-6 gap-5">
        
